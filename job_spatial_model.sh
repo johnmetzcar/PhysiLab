b9abedfc88806953c92cda02b27704059132bd6b
John Metzcar <jpmetzca@login1.hsn0.cm.bigred200.uits.iu.edu>
2024-04-02 19:14:05 -0400

    updated job scripts.

--- job_spatial_model.sh
+++ job_spatial_model.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --mail-user=jpmetzca@iu.edu
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=Template000000
+#SBATCH --job-name=spatial000000
 #SBATCH -p general
-#SBATCH -o Template_%j.txt
-#SBATCH -e Template_%j.err
-#SBATCH --nodes=6
+#SBATCH -o spatial_model_%j.txt
+#SBATCH -e spatial_model_%j.err
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=8
 #SBATCH --time=01:00:00
@@ -16,6 +16,6 @@
 module load python/3.9.8
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun -A r00241 --cpu-bind=sockets python RunSimulations_testing.py leukemia_model_files
+srun -A r00241 --cpu-bind=sockets python RunSimulations_testing.py leukemia_spatial_output
 
 #SBATCH --mem=240G
